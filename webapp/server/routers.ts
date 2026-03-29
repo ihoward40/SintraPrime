7aa2739a3a168b0b71b6cc89249e9be45c3222c8
Isiah T Howard <139932709+ihoward40@users.noreply.github.com>
2026-03-29 13:05:11 -0400

    Update routers.ts to import and register twoFactorRouter

--- webapp/server/routers.ts
+++ webapp/server/routers.ts
@@ -1,54 +1,12 @@
-import { router } from './trpc';
-import { authRouter } from './routers/auth';
-import { subscriptionRouter } from './routers/subscriptionBilling';
-import { autonomousRouter } from './autonomous/router';
-import { jurisdictionRouter } from './routers/jurisdictionRouter';
+import { AppRouter } from './app-router';
 import { twoFactorRouter } from './two-factor-router';
-import { jwtAuthRouter } from './routers/jwtAuthRouter';
-import { stripeRouter } from './routers/stripe';
-import { pacerRouter } from './routers/pacer';
-import { documentsRouter } from './routers/documents';
-import { caseFilingRouter } from './routers/caseFiling';
-import { caseActivitiesRouter } from './routers/caseActivities';
-import { casesRouter } from './routers/cases';
-import { workflowRouter } from './routers/workflow';
-import { triggerRouter } from './routers/trigger';
-import { notificationRouter } from './routers/notification';
-import { researchRouter } from './routers/research';
-import { syncRouter } from './routers/sync';
-import { adapterRouter } from './routers/adapter';
-import { legalAlertsRouter } from './routers/legalAlerts';
-import { userSettingsRouter } from './routers/userSettings';
-import { teamMembersRouter } from './routers/teamMembers';
-import { documentIntelligenceRouter } from './routers/documentIntelligence';
-import { workspacesRouter } from './routers/workspaces';
-import { notebookRouter } from './routers/notebook';
 
-export const appRouter = router({
-  auth: authRouter,
-  subscription: subscriptionRouter,
-  autonomous: autonomousRouter,
-  jurisdiction: jurisdictionRouter,
+const appRouter = new AppRouter();
+
+// Register the twoFactorRouter
+appRouter.registerRouter({
   twoFactor: twoFactorRouter,
-  jwtAuth: jwtAuthRouter,
-  stripe: stripeRouter,
-  pacer: pacerRouter,
-  documents: documentsRouter,
-  caseFiling: caseFilingRouter,
-  caseActivities: caseActivitiesRouter,
-  cases: casesRouter,
-  workflow: workflowRouter,
-  trigger: triggerRouter,
-  notification: notificationRouter,
-  research: researchRouter,
-  sync: syncRouter,
-  adapter: adapterRouter,
-  legalAlerts: legalAlertsRouter,
-  userSettings: userSettingsRouter,
-  teamMembers: teamMembersRouter,
-  documentIntelligence: documentIntelligenceRouter,
-  workspaces: workspacesRouter,
-  notebook: notebookRouter,
 });
 
-export type AppRouter = typeof appRouter;
+// Remove trkFactorRouter and troFactorRouter
+// Note: Additional logic for removing them should be placed here if needed
